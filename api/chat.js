a18c6c446d1499c3225a76483ba90735671c2e00
sirmak-io <sedairmak@gmail.com>
2026-06-20 13:19:56 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,9 +1,23 @@
-let reply;
+export default async function handler(req, res) {
 
-if (message.length < 10) {
-  reply = "Bu düşüncenizi biraz açabilir misiniz?";
-} else {
-  reply = "İlginç bir nokta. Bu görüşünüzü en çok ne şekillendirdi?";
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  const { message = "" } = req.body;
+
+  let reply;
+
+  if (message.length < 10) {
+    reply = "Bu düşüncenizi biraz açabilir misiniz?";
+  } else {
+    reply = "İlginç bir nokta. Bu görüşünüzü en çok ne şekillendirdi?";
+  }
+
+  return res.status(200).json({
+    reply
+  });
+
 }
-
-return res.status(200).json({ reply });
