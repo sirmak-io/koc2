ed768bd893c9772d33b21579cee6168bb88df02d
sirmak-io <sedairmak@gmail.com>
2026-08-22 12:53:57 +0300

    Update claude_us.js

--- api/claude_us.js
+++ api/claude_us.js
@@ -112,8 +112,8 @@ function buildOpeningMessage(data) {
     reflectionMessage = `
     <p class="reflection">
     You were right that these attitudes are uncommon among ${data.outparty} supporters. However, many supporters of your party tend to estimate them somewhat higher.  
-    Why do you think this might be the case? 
-    Could social media, news coverage, personal experiences, conversations with others, or something else have shaped your guesses? 
+   <br>
+    Why do you think this might be the case? Could social media, news coverage, personal experiences, conversations with others, or something else have shaped your guesses? 
     <br>
    There's no right answer, so please share your own thoughts.
     </p>
